572819ff13da6bcc679f0ecf45c9d80f3caf5cd3
Markel <cuestaarribas.markel@gmail.com>
2021-03-09 13:20:13 +0100

    reorganizando layout del frontpage y cambiando dislay a Field en las vistas

---
+++ web/sites/default/files/php/twig/60474b355f214_rdf-metadata.html.twig_Yy6Yy_koVGHlkX0HEUjrWqKgH/58mxv3eIDyhrwKXRv3Tp-iwRDCTK6rxH76vy503-_Oc.php
@@ -0,0 +1,96 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/modules/rdf/templates/rdf-metadata.html.twig */
+class __TwigTemplate_f1b1d2238a8672cfa4ebce83b02f1ba47b137e22b0f8a2cf0a416ad3d858449b extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["for" => 20];
+        $filters = ["escape" => 21];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['for'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 20
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(($context["metadata"] ?? null));
+        foreach ($context['_seq'] as $context["_key"] => $context["attributes"]) {
+            // line 21
+            echo "  <span";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($context["attributes"], "addClass", [0 => "hidden"], "method")), "html", null, true);
+            echo "></span>
+";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['attributes'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+    }
+
+    public function getTemplateName()
+    {
+        return "core/modules/rdf/templates/rdf-metadata.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  59 => 21,  55 => 20,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "core/modules/rdf/templates/rdf-metadata.html.twig", "/var/www/html/bilboempresas/web/core/modules/rdf/templates/rdf-metadata.html.twig");
+    }
+}
