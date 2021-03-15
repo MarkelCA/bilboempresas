b2ef60f7c863ca83cc2ea0dc452945d07ee2243c
Markel <cuestaarribas.markel@gmail.com>
2021-03-15 17:30:40 +0100

    making template for company_owner, need fix in Media Class

--- web/modules/custom/company_owner/src/Controller/ControllerOwner.php
+++ web/modules/custom/company_owner/src/Controller/ControllerOwner.php
@@ -1,10 +1,12 @@
 <?php
 namespace Drupal\company_owner\Controller;
-
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\node\Entity\NodeType;
-use Drupal\node\Entity\Node as Node;
+use Drupal\node\Entity\Node;
 use Drupal\user\Entity\User;
+use Drupal\file\Entity\File;
+use Drupal\media\Entity\Media;
+use Drupal\media\Entity;
+
 class ControllerOwner extends ControllerBase {
 
     public function edit() {
@@ -16,30 +18,45 @@ class ControllerOwner extends ControllerBase {
         $userCompany = $data['company'];
         $userName = $data['username'];
         $companyId = $data['companyId'];
+        $companyName = $data['company'];
+        $logoId = $data['logo'];
+        //var_dump(Drupal\media\Entity);
+        //$logo = \Drupal\media\Entity\Media::load($logoId);
+        //$fid = $logo->field_media_image->target_id;
+        return new \Symfony\Component\HttpFoundation\RedirectResponse("/node/$companyId/edit");
 
-        return new \Symfony\Component\HttpFoundation\RedirectResponse("/node/$companyId");
+        return array(
+            '#theme' => 'owner_editing_theme',
+            '#data' => [
+                'title' => 'Mi empresa',
+                'company' => $companyName,
+                //'imgSrc' => ''
+                //'data1' => 'algo que no es default'
+                
+            ] 
+        ); 
     }
 
    public function getUserInfo() {
         $userid = User::load(\Drupal::currentUser()->id());
-        $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
+        $user = User::load(\Drupal::currentUser()->id());
         $username = $user->get('name')->value;
         $user_company_id = $user->field_propietario_empresa->getValue()[0]['target_id'];
 
         $companyNode = \Drupal\node\Entity\Node::load($user_company_id);
+        //$company_logo = $companyNode->get('field_company_thumbnail')->value;
+        $company_logo = $companyNode->field_company_thumbnail->getValue()[0]['target_id'];
 
         if(empty($companyNode)) return;
 
         $companyTitle = $companyNode->get('title')->value;
         //$companyName = $coma
         
-        //echo 'User Company: <pre>';
-        //var_dump($companyTitle);
-        //echo '</pre>';
         return [
             'company' => $companyTitle, 
             'username' => $username, 
             'companyId' => $user_company_id, 
+            'logo' => $company_logo, 
         ];
     }
 
---
+++ web/sites/default/files/php/twig/604f8b29c915f_input.html.twig_teGw5SKDUy9ES-looGv7KmEep/seRICDeL231vt7dIVfU0rc3VuW_v5DE2Sdd-KwkjKA0.php
@@ -0,0 +1,98 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/contrib/radix/templates/form/input.html.twig */
+class __TwigTemplate_ed4849c00a340f57341383cf857eba5f14ed8853b0e79d444deab8f88c0bbed6 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["if" => 13, "set" => 14];
+        $filters = ["escape" => 17];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if', 'set'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 13
+        if ((($context["type"] ?? null) != "file")) {
+            // line 14
+            echo "  ";
+            $context["css_classes"] = [0 => "form-control"];
+        }
+        // line 16
+        echo "
+<input";
+        // line 17
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($this->getAttribute(($context["attributes"] ?? null), "addClass", [0 => ($context["css_classes"] ?? null)], "method"), "removeClass", [0 => "form-text"], "method")), "html", null, true);
+        echo " />";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["children"] ?? null)), "html", null, true);
+        echo "
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/contrib/radix/templates/form/input.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  64 => 17,  61 => 16,  57 => 14,  55 => 13,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/contrib/radix/templates/form/input.html.twig", "/var/www/html/bilboempresas/web/themes/contrib/radix/templates/form/input.html.twig");
+    }
+}
